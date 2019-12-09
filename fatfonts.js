f6b813b2874becfb3b8fcdbc29e34d5a968ecf35
YusofBandar <yusof.bandar@squaredup.com>
2019-12-09 22:04:56 +0000

    Changes let to const

--- fatfonts.js
+++ fatfonts.js
@@ -12,10 +12,10 @@ function fatfonts() {
         .attr("height", height)
         .attr("viewBox", "0 0 2000 2000");
 
-    let sizes = sizeNums(9, 9, width, height)
+    const sizes = sizeNums(9, 9, width, height)
     for (let i = 1; i < 10; i++) {
         cubica(i).then((cubica) => {
-            let coord = positionNums(i - 1, i - 1, 9, 9, width, height);
+            const coord = positionNums(i - 1, i - 1, 9, 9, width, height);
 
             let num = svg.append("g").html(cubica);
             num.select("svg")
@@ -28,21 +28,21 @@ function fatfonts() {
 }
 
 function sizeNums(xLength, yLength, width, height) {
-    let dx = width / xLength;
-    let dy = height / yLength;
+    const dx = width / xLength;
+    const dy = height / yLength;
     return [dx, dy];
 }
 
 function positionNums(x, y, xLength, yLength, width, height) {
-    let xPos = x * (width / xLength);
-    let yPos = y * (height / yLength);
+    const xPos = x * (width / xLength);
+    const yPos = y * (height / yLength);
     return [xPos, yPos];
 }
 
 
 function cubica(num) {
     const file = `cubica_${num}.svg`;
-    let path = "./fonts/cubica/";
+    const path = "./fonts/cubica/";
     return d3.text(path + file);
 }
 
