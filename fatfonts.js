e7e2b54a007e0ca4b687773e731aab6309d98b72
YusofBandar <yusof.bandar@squaredup.com>
2019-12-09 21:50:42 +0000

    Adds positioning numbers

--- fatfonts.js
+++ fatfonts.js
@@ -10,19 +10,28 @@ function fatfonts() {
         .append("svg")
         .attr("width", width)
         .attr("height", height)
-        .attr("viewBox", "0 0 1000 100");
+        .attr("viewBox", "0 0 1000 1000");
 
 
     for (let i = 1; i < 10; i++) {
         cubica(i).then((cubica) => {
+            let coord = positionNums(i-1,i-1,9,9,1000,1000);
+            
             let num = svg.append("g").html(cubica);
             num.select("svg")
-                .attr("x",100*i)
-                .attr("y",-10)
-                .attr("width",100)
-                .attr("height",100);
+                .attr("x", coord[0])
+                .attr("y", coord[1])
+                .attr("width", 100)
+                .attr("height", 100);
         })
     }
+}
+
+
+function positionNums(x, y, xLength, yLength, width, height) {
+    let xPos = x * (width / xLength);
+    let yPos = y * (height / yLength);
+    return [xPos, yPos];
 }
 
 
